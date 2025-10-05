e4d61be1b090c0c28ac387f43155698e75dcc191
DJcri <deavy.jslzr@gmail.com>
2025-10-05 01:10:54 -0700

    Reconstructed integration source files

---
+++ Source/Integrations/GenderWorksIntegration/GenderWorksIntegration/GenderWorksIntegration.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using LoveyDoveySexWithRosaline;
+using Verse;
+
+/// <summary>
+/// Simply replaces gender check of Despicable with
+/// the GenderWorks mod's
+/// </summary>
+
+namespace Despicable
+{
+    public class GenderWorksIntegration : Mod
+    {
+        public GenderWorksIntegration(ModContentPack content)
+            : base(content)
+        {
+            new Harmony("Despicable.GenderWorksIntegration").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnStateUtil), "ComparePawnGenderToByte")]
+    public static class HarmonyPatch_PawnStateUtil
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref bool __result, ref Pawn pawn, ref byte otherGender)
+        {
+            CommonUtil.DebugLog("[Gender Works] - Running gender check");
+            try
+            {
+                bool num = GenderUtilities.HasMaleReproductiveOrgan(pawn);
+                bool flag = GenderUtilities.HasFemaleReproductiveOrgan(pawn);
+                if (num && otherGender == 1)
+                {
+                    __result = true;
+                }
+                if (flag && otherGender == 2)
+                {
+                    __result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonUtil.DebugLog(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Source/Integrations/IntimacyIntegration/IntimacyIntegration/IntimacyIntegration.cs
@@ -0,0 +1,114 @@
+﻿using HarmonyLib;
+using LoveyDoveySexWithEuterpe;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+/// <summary>
+/// Patches animation into lovin' from Turkler's Intimacy mod,
+/// as well as replaces lovin' checks with Intimacy's checks.
+/// </summary>
+
+namespace Despicable
+{
+    public class IntimacyIntegration : Mod
+    {
+        public IntimacyIntegration(ModContentPack content)
+            : base(content)
+        {
+            Harmony val = new Harmony("Despicable.IntimacyIntegration");
+            val.PatchAll();
+        }
+    }
+
+    // Patch to replace lovin' checks with Intimacy's checks
+    [HarmonyPatch(typeof(LovinUtil), "PassesLovinCheck")]
+    public static class HarmonyPatch_LovinUtil
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref bool __result, ref Pawn pawn, ref Pawn target)
+        {
+            __result = true;
+            if (!SexUtilities.CanDoLovin(pawn) || !SexUtilities.CanDoLovin(target) || !SocialInteractionUtility.CanInitiateInteraction(pawn, (InteractionDef)null) || !CommonChecks.IsOldEnough(pawn) || !CommonChecks.IsOldEnough(target) || !CommonChecks.AreMutuallyAttracted(pawn, target) || CommonChecks.IdeologyForbidsLovin(pawn, target) || CommonChecks.IncestCheck(pawn, target))
+            {
+                __result = false;
+            }
+        }
+    }
+
+    // Patch to replace lovin' report text to correspond with Intimacy's checks
+    [HarmonyPatch(typeof(LovinUtil), "GetReportForLovin")]
+    public static class HarmonyPatch_LovinReport
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref string __result, ref Pawn pawn, ref Pawn pawn2)
+        {
+            __result = "[Intimacy] Pawn drafted or lovin' on cooldown";
+            if (!SocialInteractionUtility.CanInitiateInteraction(pawn, null))
+            {
+                __result = "[Intimacy] Pawn can't initiate interaction due to condition or sleeping";
+            }
+            if (!CommonChecks.IsOldEnough(pawn) || !CommonChecks.IsOldEnough(pawn2))
+            {
+                __result = "[Intimacy] Pawn isn't old enough";
+            }
+            if (!CommonChecks.AreMutuallyAttracted(pawn, pawn2))
+            {
+                __result = "[Intimacy] Pawn's aren't attracted to each other";
+            }
+            if (CommonChecks.IdeologyForbidsLovin(pawn, pawn2))
+            {
+                __result = "[Intimacy] Ideology forbids lovin'";
+            }
+            if (CommonChecks.IncestCheck(pawn, pawn2))
+            {
+                __result = "[Intimacy] Incest not desired by these pawns";
+            }
+        }
+    }
+
+    // Patch to play animation when lovin' starts
+    [HarmonyPatch(typeof(JobDriver_Sex), "BothPawnsAreHavingSex")]
+    public static class HarmonyPatch_JobDriver_SexStartAnim
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref bool __result, Pawn pawn, Pawn partner)
+        {
+            if (!__result)
+            {
+                return;
+            }
+            List<Pawn> participants = new List<Pawn> { pawn, partner };
+            CommonChecks.TryGainIntimacy(pawn, CommonChecks.IntimacyFromLovin);
+            if (!CommonUtil.GetSettings().animationExtensionEnabled)
+            {
+                return;
+            }
+            CompExtendedAnimator val = ThingCompUtility.TryGetComp<CompExtendedAnimator>((Thing)(object)pawn);
+            if ((val != null && val.hasAnimPlaying) || pawn.pather.Moving || partner.pather.Moving)
+            {
+                return;
+            }
+            val = ThingCompUtility.TryGetComp<CompExtendedAnimator>((Thing)(object)partner);
+            if (val != null && val.hasAnimPlaying)
+            {
+                return;
+            }
+            List<AnimGroupDef> playableAnimationsFor = ContextUtil.GetPlayableAnimationsFor(participants, (LovinTypeDef)null);
+            if (GenList.NullOrEmpty<AnimGroupDef>((IList<AnimGroupDef>)playableAnimationsFor))
+            {
+                return;
+            }
+            AnimGroupDef val2 = GenCollection.RandomElement<AnimGroupDef>((IEnumerable<AnimGroupDef>)playableAnimationsFor);
+            Dictionary<string, Pawn> dictionary = ContextUtil.AssignRoles(val2, participants);
+            if (dictionary != null)
+            {
+                AnimUtil.PlayAnimationGroup(val2, dictionary, (Thing)(object)pawn);
+                pawn.jobs.curDriver.AddFinishAction((Action<JobCondition>)delegate
+                {
+                    AnimUtil.ResetAnimatorsForGroup(participants);
+                });
+            }
+        }
+    }
+}
